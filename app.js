35a1d06719fb50f2e4eac9d42f7c700a764890c5
MuhdLaptop <alyami.mohammed101@gmail.com>
2023-04-28 14:47:40 +0300

    lastVersion

---
+++ app.js
@@ -0,0 +1,38 @@
+const listContainer = document.querySelector('#listContainer')
+const input = document.querySelector('#inputText')
+
+function addTask(){
+    if(input.value===''){
+        alert("You must write something!")
+    } else {
+        let li = document.createElement('li')
+        li.innerHTML=input.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span')
+        span.innerHTML='\u00d7';
+        li.appendChild(span)
+        saveTask();
+    }
+    input.value='';
+}
+
+listContainer.addEventListener('click',function(e){
+    if(e.target.tagName==='LI'){
+        e.target.classList.toggle('checked')
+        saveTask();
+    } 
+    else if(e.target.tagName==='SPAN'){
+        e.target.parentElement.remove();
+        saveTask();
+    } 
+})
+
+
+function saveTask(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem('data');
+}
+showTask();
